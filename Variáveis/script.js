206206257ea5baa8894b5f6af27b7578e5b22b8c
Yuri Herbert <55001542+Paladin-bot@users.noreply.github.com>
2022-09-08 21:00:18 -0300

    Add files via upload

---
+++ Variáveis/script.js
@@ -0,0 +1,13 @@
+// Declarar uma variável com o seu nome
+var nome = 'Yuri';
+// Declarar uma variável com a sua idade
+var idade = 27;
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comida;
+// Atribuir valor a sua comida favorita
+comida = "Frango à Parmegiana";
+// Declarar 5 variáveis diferentes sem valores
+var a, b, c, d, e;
+console.log(nome, idade, comida);
+console.log(a, b, c, d, e)
