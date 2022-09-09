21ca354aa2f5c48c1235dbc4fec66efa102a0712
Yuri Herbert <55001542+Paladin-bot@users.noreply.github.com>
2022-09-08 21:19:18 -0300

    Add files via upload

---
+++ numeros-e-operadores/script.js
@@ -0,0 +1,19 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total);
+// Crie duas expressões que retornem NaN
+var nome = "Yuri"
+var idade = '2a7'
+console.log(nome/2);
+console.log(idade/5);
+// Somar a string '200' com o número 50 e retornar 250
+var resultado = +"200" + 50;
+console.log(resultado);
+// Incremente o número 5 e retorne o seu valor incrementado
+var increment = 5;
+console.log(++increment);
+// Como dividir o peso por 2?
+var numero = +'80'/2;
+var unidade = 'kg';
+var peso = numero + unidade; 
+console.log(peso);
