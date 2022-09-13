1873f2f6dfa4c74850e235e8275a6941c525ae97
Yuri Herbert <55001542+paladinbot@users.noreply.github.com>
2022-09-12 22:36:41 -0300

    Add files via upload

---
+++ dom-para-iniciantes/o-que-e-dom/script.js
@@ -0,0 +1,11 @@
+// Retorne o url da página atual utilizando o objeto window
+const href = window.location.href;
+console.log(href);
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+firstElement = document.querySelector('.ativo');
+// Retorne a linguagem do navegador
+const language = window.navigator.language;
+console.log(language);
+// Retorne a largura da janela 
+const windowWidth = window.innerWidth;
