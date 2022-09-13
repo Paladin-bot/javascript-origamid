f79bb84e2660eb1c6eefde7508039e21078814dd
Yuri Herbert <55001542+paladinbot@users.noreply.github.com>
2022-09-12 22:35:40 -0300

    Add files via upload

---
+++ javascript-para-iniciantes/boolean-e-condicionais/script.js
@@ -0,0 +1,36 @@
+// Verifique se a sua idade é maior do que a de algum parente
+// Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
+
+// Qual valor é retornado na seguinte expressão?
+var expressao = (5 - 1) && (5 - ' ') && (5 - 7);
+console.log(expressao)
+// Verifique se as seguintes variáveis são Truthy ou Falsy
+var nome = 'Andre';
+var idade = 28;
+var possuiDoutorado = false;
+var empregoFuturo;
+var dinheiroNaConta = 0;
+console.log(!!nome, !!idade, !!possuiDoutorado, !!empregoFuturo, !!dinheiroNaConta);
+
+// Compare o total de habitantes do Brasil com China (valor em milhões)
+var brasil = 207;
+var china = 1340;
+
+if(brasil > china) {
+  console.log('Brasil maior que China');
+} else {
+  console.log('China maior que Brasil');
+}
+// O que irá aparecer no console?
+if(('Gato' === 'gato') && (5 > 2)) {
+  console.log('Verdadeiro');
+} else {
+  console.log('Falso');
+}
+
+// O que irá aparecer no console?
+if(('Gato' === 'gato') || (5 > 2)) {
+  console.log('Gato' && 'Cão');
+} else {
+  console.log('Falso');
+}
---
+++ javascript-para-iniciantes/objetos/script.js
@@ -0,0 +1,35 @@
+// Crie um objeto com os seus dados pessoais
+// Deve possui pelo menos duas propriedades nome e sobrenome
+var dados = {
+  nome : "Yuri",
+  sobrenome : "Tavares",
+  idade : 27
+}
+console.log(dados.nome, dados.sobrenome, dados.idade)
+// Crie um método no objeto anterior, que mostre o seu nome completo
+dados.nomecompleto = function(){
+  return this.nome + ' ' + this.sobrenome;
+};
+dados.nome = 'Davi';
+// Modifique o valor da propriedade preco para 3000
+var carro = {
+  preco: 1000,
+  portas: 4,
+  marca: 'Audi',
+}
+
+carro.preco = 3000;
+// Crie um objeto de um cachorro que represente um labrador,
+// preto com 10 anos, que late ao ver um homem
+var cachorro = {
+  raca : 'Labrador',
+  cor: 'preto',
+  idade: '10 anos',
+  latir(pessoa){
+    if(pessoa === 'homem'){
+      return 'latir';
+    } else {
+      return 'nada';
+    }
+  }
+}
